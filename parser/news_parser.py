306907626e70eb59c1782a6d0352015595013ef2
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-08-10 00:46:49 +0200

    moved tests, moved news_parser main method code to process.py

--- parser/news_parser.py
+++ parser/news_parser.py
@@ -218,30 +218,3 @@ class SpeyerParser(NewsParser):
                    "content": content}
 
         return article
-
-
-if __name__ == '__main__':
-    blog_parsers_classes = [BVDGParser, SpeyerParser, SchwedtParser]
-
-    headers = ["Blog Name", "Heading", "Date", "Image", "Content"]
-    table = list()
-    for blog_parser_class in blog_parsers_classes:
-        blog_parser_instance = blog_parser_class()
-        page = urllib2.urlopen(blog_parser_class.ARTICLES_URL + "1").read()
-        first_article_url = blog_parser_instance.parse_article_urls(page)[0]
-        article = blog_parser_class.parse_article_from_url(first_article_url)
-        row = [blog_parser_instance.BLOG_NAME,
-               "[{}]({})".format(article["heading"], article["url"]),
-               datetime.fromtimestamp(float(article["date"])).strftime("%Y-%m-%d"),
-               "<img src='{}' width='100px'/>".format(article["image"]) if article["image"] else "",
-               article["content"][:20] + "..."
-               ]
-        table.append(row)
-    table = sorted(table, key=lambda k: datetime.strptime(k[2], "%Y-%m-%d"), reverse=True)
-
-    with codecs.open("README.md", 'r', encoding='utf8') as f:
-        readme = f.read()
-    before_news = readme.split("## Current News")[0]
-    new_readme = before_news + "\n## Current News\n\n" + tabulate(table, headers, tablefmt="pipe")
-    with codecs.open("README.md", 'w', encoding='utf8') as f:
-        f.write(new_readme)
--- process.py
+++ process.py
@@ -1,4 +1,33 @@
-from buli_parser import BuliParser
+import codecs
+import urllib2
+from datetime import datetime
+from parser.buli_parser import BuliParser
+from parser.news_parser import BVDGParser, SpeyerParser, SchwedtParser
+from tabulate import tabulate
+
+def update_readme(blog_parsers_classes):
+    headers = ["Blog Name", "Heading", "Date", "Image", "Content"]
+    table = list()
+    for blog_parser_class in blog_parsers_classes:
+        blog_parser_instance = blog_parser_class()
+        page = urllib2.urlopen(blog_parser_class.ARTICLES_URL + "1").read()
+        first_article_url = blog_parser_instance.parse_article_urls(page)[0]
+        article = blog_parser_class.parse_article_from_url(first_article_url)
+        row = [blog_parser_instance.BLOG_NAME,
+               "[{}]({})".format(article["heading"], article["url"]),
+               datetime.fromtimestamp(float(article["date"])).strftime("%Y-%m-%d"),
+               "<img src='{}' width='100px'/>".format(article["image"]) if article["image"] else "",
+               article["content"][:20] + "..."
+               ]
+        table.append(row)
+    table = sorted(table, key=lambda k: datetime.strptime(k[2], "%Y-%m-%d"), reverse=True)
+
+    with codecs.open("README.md", 'r', encoding='utf8') as f:
+        readme = f.read()
+    before_news = readme.split("## Current News")[0]
+    new_readme = before_news + "## Current News\n\n" + tabulate(table, headers, tablefmt="pipe")
+    with codecs.open("README.md", 'w', encoding='utf8') as f:
+        f.write(new_readme)
 
 if __name__ == '__main__':
     SEASON = "1516"
@@ -10,3 +39,6 @@ if __name__ == '__main__':
 
     for parser in [BuliParser1A, BuliParser1B, BuliParser2South, BuliParser2North, BuliParser2Middle]:
         parser.create_buli_files()
+
+    blog_parsers_classes = [BVDGParser, SpeyerParser, SchwedtParser]
+    update_readme(blog_parsers_classes)
