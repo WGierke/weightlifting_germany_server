6557964ed7189fd952487e01c4e7729bf4b164fd
Willi Gierke <willi.gierke@student.hpi.de>
2017-10-12 10:19:01 +0200

    Support season 17/18

--- process.py
+++ process.py
@@ -7,17 +7,16 @@ from utils import update_readme, commit_changes, send_to_slack, get_endpoint, is
 if __name__ == '__main__':
     print "Endpoint: " + get_endpoint()
 
-    SEASON = "1617"
+    SEASON = "1718"
     BuliParser1A = BuliParser(SEASON, "1", "Gruppe+A", "1. Bundesliga - Staffel A", "4", leage_relay="1a")
     BuliParser1B = BuliParser(SEASON, "1", "Gruppe+B", "1. Bundesliga - Staffel B", "5", leage_relay="1b")
     BuliParser2A = BuliParser(SEASON, "2", "Gruppe+A", "2. Bundesliga - Staffel A", "7", leage_relay="2a")
     BuliParser2B = BuliParser(SEASON, "2", "Gruppe+B", "2. Bundesliga - Staffel B", "8", leage_relay="2b")
-    BuliParser2C = BuliParser(SEASON, "2", "Gruppe+C", "2. Bundesliga - Staffel C", "9", leage_relay="2c")
 
     blog_parsers_instances = [BVDGParser(), SpeyerParser(), SchwedtParser(), MutterstadtParser()]
 
     try:
-        for parser in [BuliParser1A, BuliParser1B, BuliParser2A, BuliParser2B, BuliParser2C]:
+        for parser in [BuliParser1A, BuliParser1B, BuliParser2A, BuliParser2B]:
             parser.update_buli()
 
         for blog_parser_instance in blog_parsers_instances:
