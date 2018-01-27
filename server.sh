55de9d8141a0e933feeb508bcb6ee18bd09f45ea
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2018-01-27 22:18:03 +0100

    Do not loop

--- server.sh
+++ server.sh
@@ -1,8 +1,4 @@
-while true
-do
-	date +"%T"
-	git fetch --all
-	git reset --hard origin/master
-	python process.py --notify
-	sleep 3600 #60 minutes
-done
+date +"%T"
+git fetch --all
+git reset --hard origin/master
+python process.py --notify
